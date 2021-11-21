eb127cdaa179274b33f812a705b3a36345df6d52
Evangelivm <devysalomon@gmail.com>
2021-11-20 20:48:59 -0500

    changes

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -28,6 +28,6 @@ document.querySelector('#date').innerHTML = days[date.getDay()] + ", " + date.ge
 function toggleMenu(){
     document.getElementById("primaryNav").classList.toggle("hide");
 }
-if (date.getDay() != 6) {
+if (date.getDay() != 5) {
     document.getElementById("banner-fridays").style.display = "none"
 }
