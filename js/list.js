5022556454632438bd370ec47c93c57cbba777a8
Devy Salomon <devysalomon@gmail.com>
2023-07-11 00:05:51 -0500

    new

--- js/list.js
+++ js/list.js
@@ -1,40 +1,66 @@
 fetch("data.json")
   .then(response => response.json())
   .then(data => {
-    // Iterar sobre los elementos del JSON
-    data.result.forEach((item, index) => {  // Acceder a la propiedad "result" del objeto JSON
-      const cardImage = document.querySelectorAll(".card__image")[index];
-      const genre = document.querySelectorAll(".tag")[index];
-      const title = document.querySelectorAll("h2")[index];
-      const plot = document.querySelectorAll("p")[index];
-      const platform = document.querySelectorAll(".user__info2 h5")[index];
+    const container = document.querySelector('.container');
 
-      // Actualizar el contenido de los elementos HTML con los datos del JSON actual
-      cardImage.src = item.posterURLs['500'];  // Usar la propiedad "posterURLs" del objeto JSON
-      genre.textContent = item.genres[0]['name'];  // Usar la propiedad "tagline" del objeto JSON
-      title.textContent = item.title;
-      plot.textContent = item.overview;  // Usar la propiedad "overview" del objeto JSON
-      // Mostrar el contenido de "streamingInfo" en "platform"
-      const streamingInfo = item.streamingInfo;
-      if (streamingInfo && streamingInfo.us) {
-        const platforms = Object.keys(streamingInfo.us); // Obtener las plataformas disponibles en la región "us"
-        let uniquePlatforms = Array.from(new Set(platforms)); // Eliminar las plataformas duplicadas
-        let platformHTML = "";
-        uniquePlatforms.forEach(platform => {
-          const services = streamingInfo.us[platform];
-          const firstService = services[0]; // Obtener el primer servicio de la plataforma
-          const serviceName = platform.charAt(0).toUpperCase() + platform.slice(1);
-          const serviceLink = firstService.link;
-          platformHTML += `<a href="${serviceLink}" target="_blank">${serviceName}</a>, `;
-        });
-        platform.innerHTML = platformHTML.slice(0, -2); // Eliminar la coma y el espacio extra al final
-      } else {
-        platform.textContent = "No disponible";
-      }
+    data.result.forEach(item => {
+      // Crear el elemento <div class="card">
+      const card = document.createElement('div');
+      card.classList.add('card');
+
+      // Crear el contenido interno del elemento <div class="card">
+      const cardContent = `
+        <div class="card__header">
+          <img src="${item.posterURLs['342']}" alt="card__image" class="card__image" width="600">
+        </div>
+        <div class="card__body2">
+          <span class="tag tag-red">${item.genres[0]['name']}</span>
+          <h2>${item.title}</h2>
+          <h3>Plot</h3>
+          <p>${item.overview}</p>
+        </div>
+        <div class="card__middle">
+          <div class="user">
+            <div class="user__info">
+              <h5>Available at:</h5>
+            </div>
+          </div>
+        </div>
+        <div class="card__footer">
+          <div class="user">
+            <div class="user__info2">
+              <h5>${getPlatformLinks(item.streamingInfo)}</h5>
+            </div>
+          </div>
+        </div>
+      `;
+
+      // Asignar el contenido al elemento <div class="card">
+      card.innerHTML = cardContent;
+
+      // Agregar el elemento <div class="card"> al contenedor
+      container.appendChild(card);
     });
   })
   .catch(error => {
     console.error('Error al cargar el JSON:', error);
   });
+
+function getPlatformLinks(streamingInfo) {
+  if (streamingInfo && streamingInfo.us) {
+    const platforms = Object.keys(streamingInfo.us);
+    const uniquePlatforms = Array.from(new Set(platforms));
+    let platformHTML = "";
+    uniquePlatforms.forEach(platform => {
+      const services = streamingInfo.us[platform];
+      const firstService = services[0];
+      const serviceName = platform.charAt(0).toUpperCase() + platform.slice(1);
+      const serviceLink = firstService.link;
+      platformHTML += `<a href="${serviceLink}" target="_blank">${serviceName}</a>, `;
+    });
+    return platformHTML.slice(0, -2);
+  }
+  return "No disponible";
+}
 
 
